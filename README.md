a77679d30e599a1029778321298cde64cb6af386
Nolwenn Rousseau <nolwenn.rousseau@kelenntech.com>
2024-02-15 11:12:14 +0100

    git: initial commit ​

---
+++ README.md
@@ -0,0 +1,87 @@
+# Projet de dithering
+
+## 1. Contexte
+
+Dans le contexte de traitement d'images numériques, le dithering est une technique utilisée pour réduire les artefacts visuels, tels que le "banding" ou l'effet de "moire", qui peuvent apparaître lors de la réduction du nombre de couleurs dans une image.
+
+Lorsque vous réduisez le nombre de couleurs dans une image, par exemple lors de la conversion d'une image haute résolution en une image avec moins de couleurs pour une utilisation sur le web ou dans des applications mobiles, vous risquez de perdre des détails et de créer des transitions abruptes entre les couleurs adjacentes. Cela peut entraîner l'apparition de bandes de couleurs visibles, en particulier dans les dégradés de couleur.
+
+Le dithering consiste à ajouter un léger bruit aléatoire à l'image pour créer l'illusion de plus de couleurs ou de nuances, même si le nombre de couleurs réelles reste limité. Ce bruit aléatoire aide à masquer les transitions abruptes entre les couleurs et à rendre les dégradés de couleur plus lisses et plus naturels.
+
+Voici un exemple : la **_Figure 1_** représente l'image originale, tandis que la **_Figure 2_** montre l'image après l'application d'un algorithme de dithering.
+
+<img src="./figure/001.png" alt="Figure 1 : Image d'origine" style="height: 100px"/>
+
+Figure 1 : Image d'origine
+
+<img src="./figure/002.png" alt="Figure 2 : Image avec dithering" style="height: 100px"/>
+
+Figure 2 : Image avec dithering
+
+## 2. Cmyk Image
+
+CMYK et RGB sont deux modèles de couleur utilisés dans la reproduction des couleurs, mais ils sont utilisés dans des contextes différents.
+
+CMYK fait référence aux couleurs primaires utilisées dans l'impression : Cyan, Magenta, Jaune et Noir (Key).
+Ce modèle est largement utilisé dans l'industrie de l'impression, où les couleurs sont superposées en couches pour créer une gamme étendue de teintes.
+Dans notre modèle CMYK, chaque couleur est représentée par une valeur, allant de **0** (absence de couleur) à **255** (couleur pleine). En combinant différentes quantités de ces quatre encres, il est possible de reproduire une grande variété de couleurs.
+
+Ainsi, chaque couleur est représentée par un octet, et l'ordre des couleurs est le suivant : cyan (C), magenta (M), jaune (Y) et noir (K). Pour une image de dimensions, par exemple, de 50 pixels de large et 50 pixels de haut, la taille de cette image devrait être de 10 000 octets.
+
+## 3. Objectifs du Projet
+
+### 3.1 Compresser les couleurs
+
+Comme expliqué dans la partie 2, chaque pixel est représenté par 4 octets. L'objectif du projet est d'utiliser un seul octet pour représenter quatre couleurs différentes. Ainsi, nous attribuons les deux premiers bits pour représenter la couleur C, les 2 bits suivants pour la couleur M, et ainsi de suite, comme illustré dans la **_Figure 3_**.
+
+<img src="./figure/003.png" alt="Figure 3 : Utilisation de 8 bits pour représenter 32 bits" style="height: 100px"/>
+
+Figure 3 : Utilisation de 8 bits pour représenter 32 bits
+
+Si chaque couleur représente un canal, une image RGB comporte trois canaux, tandis qu'une image CMYK comporte quatre canaux. Donc pour chaque canal, il existe quatre types d'intensité, notés 0, 1, 2 et 3.
+
+Dans le fichier _main.cpp_, un tableau nommé **_thresholds_** est présent. Nous pouvons exploiter les valeurs de ce tableau pour compresser les couleurs.
+
+### 3.2 Dithering image
+
+La partie la plus cruciale de ce projet consiste à élaborer un algorithme de dithering. Par conséquent, dans le fichier main.cpp, votre seule tâche consistera à écrire la fonction **_ditheringAlgo_**. Vous n'aurez pas besoin de modifier d'autres parties du code, mais vous êtes autorisé à le faire si nécessaire, en ajoutant des commentaires expliquant les raisons de ces modifications.
+
+```
+void ditheringAlgo(uint8_t* in, int width, int height, int* thresholds, uint8_t* out)
+{
+    //TODO
+}
+```
+
+(Indice : the size of **_in_** : 4 * width * height, the size of **_out_** : width * height)
+
+**Attention : Ajoutez des commentaires aux codes que vous avez écrits.**
+
+## 4. Environnement d'exécution
+
+4.1 Compilation : g++
+Vous pouvez vérifier si vous avez déjà installé g++ en utilisant la commande "g++ --version". Si ce n'est pas le cas, vous devrez d'abord l'installer.
+
+4.2 Choix de l'éditeur : Vous êtes libre de choisir l'éditeur qui vous convient le mieux. Cependant, nous recommandons **_Visual Studio Code_**.
+
+## 5. Compiler et tester
+
+5.1 Commande de compilation : ```g++ main.cpp -o main```
+
+Toutes les images de test se trouvent dans le répertoire "images", tandis que les résultats de l'algorithme se situent dans le répertoire "results/images". Dans le répertoire "results/example", vous trouverez des exemples de résultats obtenus à partir d'un algorithme de dithering.
+
+5.2	Commande pour exécuter sur une image, par exemple test0.png : ```.\main.exe .\images\test0.png```
+
+## 6. Intérêt du projet
+
+KELENN Technology est une entreprise industrielle dans le domaine des systèmes de vision, d’impression à jet d’encre et de logiciels. Dans notre système d'impression, un élément crucial est l'utilisation de l'algorithme de dithering pour prétraiter les images. Les avantages de cette approche sont multiples :
+
+•	Optimisation de la qualité d'impression : Le tramage permet d'adapter les images à la résolution de sortie de l'imprimante, garantissant ainsi une meilleure qualité visuelle.
+
+•	Économie de ressources : En convertissant les images en motifs de points, l'algorithme de dithering réduit la quantité d'encre ou de toner nécessaire à l'impression, ce qui peut contribuer à des économies de coûts significatives.
+
+•	Correction des variations de couleur : Le dithering peut aider à atténuer les variations de couleur ou de luminosité dans les images, assurant ainsi une reproduction plus fidèle des tons et des dégradés.
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,230 @@
+#include <stdint.h>
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#define CHANNEL_RGB 3
+#define CHANNEL_CMYK 4
+
+
+void ditheringAlgo(uint8_t* in, int width, int height, int* thresholds, uint8_t* out)
+{
+    //TODO
+}
+
+uint8_t lookupTable(uint8_t val)
+{
+    if(val == 0) return 0;
+    else if(val == 1) return 33;
+    else if(val == 2) return 66;
+    else return 100;
+}
+
+void cmykToRgb(uint8_t cmyk, uint8_t* rgb)
+{
+    uint8_t mask_c = 192;  //0b11000000
+    uint8_t mask_m = 48;   //0b00110000
+    uint8_t mask_y = 12;   //0b00001100
+    uint8_t mask_k = 3;    //0b00000011
+
+    uint8_t c = lookupTable(mask_c & cmyk);
+    uint8_t m = lookupTable(mask_m & cmyk);
+    uint8_t y = lookupTable(mask_y & cmyk);
+    uint8_t k = lookupTable(mask_k & cmyk);
+
+    rgb[0] = 255 * (100 - c) * (100 - k) / 10000;       // R = 255*(100-C)*(100-K)/10000；
+    rgb[1] = 255 * (100 - m) * (100 - k) / 10000;       // G = 255*(100-M)*(100-K)/10000;
+    rgb[2] = 255 * (100 - y) * (100 - k) / 10000;       // B = 255*(100-Y)*(100-K)/10000;
+}
+
+void compressedCmykImgToRgbImg(uint8_t *compressedCmyk, uint8_t* rgbImg, int height, int width)
+{
+    uint8_t rgb[3] = {0};
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            cmykToRgb(*(compressedCmyk + i * width + j), rgb);
+
+            *(rgbImg + i * width * CHANNEL_RGB + (CHANNEL_RGB * j + 0)) = rgb[0];
+            *(rgbImg + i * width * CHANNEL_RGB + (CHANNEL_RGB * j + 1)) = rgb[1];
+            *(rgbImg + i * width * CHANNEL_RGB + (CHANNEL_RGB * j + 2)) = rgb[2];
+        }
+    }
+}
+
+void rgbToCmyk(uint8_t r, uint8_t g, uint8_t b, uint8_t* c, uint8_t* m, uint8_t* y, uint8_t* k)
+{
+    double dr = (double)r / 255.0;
+    double dg = (double)g / 255.0;
+    double db = (double)b / 255.0;
+
+    double dk = 1 - std::max(std::max(dr, dg), db);
+    double dc = (1 - dr - dk) / (1 - dk);
+    double dm = (1 - dg - dk) / (1 - dk);
+    double dy = (1 - db - dk) / (1 - dk);
+
+    *k = (uint8_t) (dk * 255);
+    *c = (uint8_t) (dc * 255);
+    *m = (uint8_t) (dm * 255);
+    *y = (uint8_t) (dy * 255);
+}
+
+void rgbImgToCmykImg(uint8_t* input, uint8_t* cmyk_image, int width, int height)
+{
+
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            uint8_t r = *(input + i * width * CHANNEL_RGB + (CHANNEL_RGB * j + 0));
+            uint8_t g = *(input + i * width * CHANNEL_RGB + (CHANNEL_RGB * j + 1));
+            uint8_t b = *(input + i * width * CHANNEL_RGB + (CHANNEL_RGB * j + 2));
+
+            rgbToCmyk(r, g, b, (cmyk_image + i * width * CHANNEL_CMYK + (CHANNEL_CMYK * j + 0)), \
+                               (cmyk_image + i * width * CHANNEL_CMYK + (CHANNEL_CMYK * j + 1)), \
+                               (cmyk_image + i * width * CHANNEL_CMYK + (CHANNEL_CMYK * j + 2)), \
+                               (cmyk_image + i * width * CHANNEL_CMYK + (CHANNEL_CMYK * j + 3)));
+
+        }
+    }
+}
+
+// -------------------------------------------------------------------------------------------------------- //
+// BMP Header  source: https://github.com/lambda-pixel/tuto-youtube/blob/01-bmp/main.cpp
+
+#pragma pack(1)
+struct BMP_Header
+{
+    uint8_t  type[2];
+    uint32_t file_size;
+    uint8_t  reserved[4];
+    uint32_t offset_start_framebuffer;
+};
+
+#pragma pack(1)
+struct BMP_Info_Header
+{
+    uint32_t header_size;
+    uint32_t width;
+    uint32_t height;
+    uint16_t n_color_planes;
+    uint16_t bits_per_pixel;
+    uint32_t compression_method;
+    uint32_t raw_size_framebuffer;
+    int32_t  h_res;
+    int32_t  v_res;
+    uint32_t n_color_palettes;
+    uint32_t n_important_colors;
+};
+
+
+struct BMP_Color
+{
+    uint8_t b;
+    uint8_t g;
+    uint8_t r;
+    uint8_t _;
+};
+
+// -------------------------------------------------------------------------------------------------------- //
+
+
+int main(int argc, char *argv[]) {
+
+    if(argc != 2)
+    {
+        std::cout << "The number of the argument is not 2; perhaps you forgot the name of an image." << std::endl;
+        return 0;
+    }
+
+    int width = 0, height = 0, bpp = 0;
+
+    int thresholds[4] = {0, 100, 180, 255};
+
+    uint8_t* input = stbi_load(argv[1], &width, &height, &bpp, 3);
+
+    std::cout << "The width of the image is:  " << width <<  "." << std::endl;
+    std::cout << "The height of the image is: " << height << "." << std::endl;
+
+    uint8_t* cmyk_image = (uint8_t*)malloc(width * height * CHANNEL_CMYK);
+
+    rgbImgToCmykImg(input, cmyk_image, width, height);
+
+    uint8_t* output = (uint8_t*)malloc(width * height);
+
+    ditheringAlgo(cmyk_image, width, height, thresholds, output);    
+
+    std::string fileNa = std::string(argv[1]);
+    std::string outputFile = "results/" + fileNa + ".bmp";
+
+    uint8_t* outputImage = (uint8_t*)malloc(width * height * CHANNEL_RGB);
+
+    compressedCmykImgToRgbImg(output, outputImage, height, width);
+
+    char imgName[100];
+
+    strcpy(imgName, outputFile.c_str()); 
+
+    // Specification des donnees des headers
+    const uint32_t size_framebuffer = width * height * sizeof(BMP_Color);
+
+    const BMP_Header bmp_header = {
+        .type                     = {'B', 'M'},
+        .file_size                = sizeof(BMP_Header) + sizeof(BMP_Info_Header) + size_framebuffer,
+        .reserved                 = {0},
+        .offset_start_framebuffer = sizeof(BMP_Header) + sizeof(BMP_Info_Header)
+    };
+
+    const BMP_Info_Header info_header = {
+        .header_size          = sizeof(BMP_Info_Header),
+        .width                = (uint32_t)width,
+        .height               = (uint32_t)height,
+        .n_color_planes       = 1,
+        .bits_per_pixel       = 8 * sizeof(BMP_Color),
+        .compression_method   = 0,
+        .raw_size_framebuffer = size_framebuffer,
+        .h_res                = 2835,
+        .v_res                = 2835,
+        .n_color_palettes     = 0,
+        .n_important_colors   = 0
+    };
+
+    BMP_Color *framebuffer = new BMP_Color[width * height];
+
+    for (int i = height - 1; i >= 0; i--) {
+        for (int j = 0; j < width; j++) {
+            framebuffer[(height - 1 - i) * width + j].r = *(outputImage + i * width * CHANNEL_RGB + (CHANNEL_RGB * j + 0));
+            framebuffer[(height - 1 - i) * width + j].g = *(outputImage + i * width * CHANNEL_RGB + (CHANNEL_RGB * j + 1));
+            framebuffer[(height - 1 - i) * width + j].b = *(outputImage + i * width * CHANNEL_RGB + (CHANNEL_RGB * j + 2));
+        }
+    }
+
+    std::FILE * f_out = std::fopen(imgName, "wb");
+
+    if (f_out != NULL) {
+
+        std::fwrite(&bmp_header, sizeof(BMP_Header), 1, f_out);
+        std::fwrite(&info_header, sizeof(BMP_Info_Header), 1, f_out);
+        std::fwrite(framebuffer, sizeof(BMP_Color), width * height, f_out);
+
+        std::fclose(f_out);
+    }  
+
+    delete[] framebuffer;
+
+    free(cmyk_image);
+    free(output);
+    free(outputImage);
+    stbi_image_free(input);
+
+    return 0;
+}
